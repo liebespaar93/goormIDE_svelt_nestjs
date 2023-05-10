fcb50aad0c15810183d725053284be7057cb29fc
kyoulee <kyoulee@bases-MacBook-Air.local>
2023-05-10 09:55:53 +0900

    lesson 2 movie

---
+++ nest_js/new-nest/src/movies/movies.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+
+@Controller('movies')
+export class MoviesController {
+
+	constructor(readonly moviesService: MoviesService){};
+
+	@Get()
+	getAll(){
+		return this.moviesService.getAll();
+	}
+
+	@Get(":id")
+	getOne(@Param('id') movieId: string): Movie{
+		console.log("Get : call => ", movieId);
+		return this.moviesService.getOne(movieId);
+	}
+
+	@Post()
+	create(@Body() movieData) {
+		console.log(movieData);
+		return this.moviesService.create(movieData);
+	}
+
+	@Delete("/:id")
+	remove(@Param('id') movieId: string) {
+		return this.moviesService.deleteOne(movieId)
+	}
+	@Patch("/:id")
+	patch(@Param('id') movieId: string, @Body() updateData) {
+		console.log("Path : call => ",updateData);
+		return this.moviesService.update(movieId, updateData);
+	}
+}
